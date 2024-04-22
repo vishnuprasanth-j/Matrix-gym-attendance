07a06365cdd60b6fe30a88a7e330636555779516
vishnuprasanth-j <jvpvis6@gmail.com>
2024-04-22 17:31:27 +0530

    updated dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -145,15 +145,14 @@ const DashboardPage = () => {
         Logout
         <FontAwesomeIcon icon={faArrowRightFromBracket} />
       </Button>
-      {/* <Grid container spacing={3}>
+      <Grid container spacing={3}>
         <Grid item xs={12} sm={4} style={{ width: "100%", height: "300px" }}>
           <canvas ref={genderChartRef}></canvas>
         </Grid>
         <Grid item xs={12} sm={4} style={{ width: "100%", height: "300px" }}>
           <canvas ref={statusChartRef}></canvas>
         </Grid>
-      </Grid> */}
-      
+      </Grid>
     </div>
   );
 };
