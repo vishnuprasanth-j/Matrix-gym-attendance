a4220696d26eef960bd8a52482c42491b7188974
vishnuprasanth-j <jvpvis6@gmail.com>
2024-05-07 11:36:45 +0530

    updated dashboard page

---
+++ src/components/SidebarDashboard.jsx
@@ -0,0 +1,87 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Drawer,
+  List,
+  ListItemText,
+  ListItemButton,
+  Backdrop,
+  IconButton,
+} from "@mui/material";
+import { SignOutUser } from "../lib/firebase";
+import { useContext } from "react";
+import { AuthContext } from "../lib/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SidebarDashboard = ({ isOpen, handleClose }) => {
+    const navigate = useNavigate();
+    const { branch } = useParams();
+    const { setCurrentUser } = useContext(AuthContext);
+    const handleSignOut = async () => {
+        try {
+          await SignOutUser();
+          navigate("/");
+          setCurrentUser();
+          handleClose();
+        } catch (error) {
+          console.error("Error signing out:", error);
+        }
+      };
+    
+    return(<>
+          <Backdrop
+            sx={{ zIndex: (theme) => theme.zIndex.drawer - 1 }}
+            open={isOpen}
+            onClick={handleClose}
+          />
+          <Drawer
+            open={isOpen}
+            onClose={handleClose}
+            sx={{
+              width: 250,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: 250,
+              },
+            }}
+          >
+            <IconButton
+              sx={{ position: "absolute", top: 0, left: 0 }}
+              onClick={handleClose}
+            >
+              <FontAwesomeIcon icon={faTimes} />
+            </IconButton>
+    
+            <List
+              sx={{
+                marginTop: "50px",
+              }}
+            >
+              <ListItemButton
+                component={Link}
+                to={`/Dashboard/Earnings`}
+                onClick={handleClose}
+              >
+                <ListItemText primary="Earnings" />
+              </ListItemButton>
+              <ListItemButton
+                component={Link}
+                to={`/Dashboard/Statistics`}
+                onClick={handleClose}
+              >
+                <ListItemText primary="Stats" />
+              </ListItemButton>
+            <ListItemButton onClick={handleSignOut}>
+                <ListItemText primary="Logout" />
+                <FontAwesomeIcon icon={faArrowRightFromBracket} />
+              </ListItemButton>
+            </List>
+          </Drawer>
+        </>
+    );
+};
+
+export default SidebarDashboard
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,67 +4,109 @@ import Chart from "chart.js/auto";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../lib/AuthContext";
 import { SignOutUser, db } from "../lib/firebase";
-import { Button, Grid } from "@mui/material";
+import { Button, CircularProgress, Grid, MenuItem, Select } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRightFromBracket,
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
-import Sidebar from "../components/SideBar";
+import SidebarDashboard from "../components/SidebarDashboard";
+import "../styles/DashboardPage.css";
 
 const DashboardPage = () => {
   const navigate = useNavigate();
   const { setCurrentUser } = useContext(AuthContext);
-  const [maleCount, setMaleCount] = useState(0);
-  const [femaleCount, setFemaleCount] = useState(0);
-  const [activeCount, setActiveCount] = useState(0);
-  const [expiredCount, setExpiredCount] = useState(0);
-  const genderChartRef = useRef(null);
-  const statusChartRef = useRef(null);
-  const genderChartInstance = useRef(null);
-  const statusChartInstance = useRef(null);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [jyData, setJyData] = useState({
+    maleCount: 0,
+    femaleCount: 0,
+  });
+  const [ksData, setKsData] = useState({
+    maleCount: 0,
+    femaleCount: 0,
+  });
 
+  const jyChartRef = useRef(null);
+  const ksChartRef = useRef(null);
+  const jyChartInstance = useRef(null);
+  const ksChartInstance = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [monthlyEarnings, setMonthlyEarnings] = useState(Array(12).fill(0));
+  const earningsChartRef = useRef(null);
+  const earningsChartInstance = useRef(null);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(()=>{
+    setLoading(true)
+    const fetchData = async () => {
+      let memData = [];
+      try {
+        const localMembers = localStorage.getItem("absentees");
+        if (localMembers) {
+          memData = JSON.parse(localMembers);
+        } else {
+          const querySnapshot = await getDocs(collection(db, "members"));
+          querySnapshot.forEach((doc) => {
+            memData.push({ id: doc.id, ...doc.data() });
+          });
+          localStorage.setItem("absentees",JSON.stringify(memData))
+        }
+        const startOfYear = new Date(selectedYear, 0, 1);
+        const endOfYear = new Date(selectedYear, 11, 31);
+        let earningsByMonth = Array(12).fill(0);
+        memData.forEach((member) => {
+          member.planHistory?.forEach((plan) => {
+            const start = new Date(plan.planStart.seconds * 1000);
+            const end = new Date(plan.planEnd.seconds * 1000);
+            const amount = parseFloat(plan.amount);
+
+            if (start >= startOfYear && end <= endOfYear) {
+              const month = start.getMonth();
+              earningsByMonth[month] += amount;
+            }
+          });
+        });
+        setMonthlyEarnings(earningsByMonth);       
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      setLoading(false)
+    };
+
+    fetchData();
+  },[selectedYear])
   useEffect(() => {
     const fetchData = async () => {
+      let memData = [];
       try {
-        const querySnapshot = await getDocs(collection(db, "members"));
-        let maleCount = 0;
-        let femaleCount = 0;
-        let activeCount = 0;
-        let expiredCount = 0;
+        const localMembers = localStorage.getItem("absentees");
+        if (localMembers) {
+          memData = JSON.parse(localMembers);
+        } else {
+          const querySnapshot = await getDocs(collection(db, "members"));
+          querySnapshot.forEach((doc) => {
+            memData.push({ id: doc.id, ...doc.data() });
+          });
+        }
+        let jyMale = 0,
+          jyFemale = 0,
+          ksMale = 0,
+          ksFemale = 0;
+        memData.forEach((doc) => {
+          const { gender, branch } = doc;
 
-        querySnapshot.forEach((doc) => {
-          const { gender, currentPlan, currPlanStart } = doc.data();
-          if (gender === "Male") {
-            maleCount++;
-          } else if (gender === "Female") {
-            femaleCount++;
-          }
-
-          const planDuration = {
-            plan1: 1,
-            plan2: 4,
-            plan3: 6,
-            plan4: 12,
-          };
-
-          const expirationDate = new Date(currPlanStart.toDate());
-          expirationDate.setMonth(
-            expirationDate.getMonth() + planDuration[currentPlan]
-          );
-
-          if (expirationDate < new Date()) {
-            expiredCount++;
-          } else {
-            activeCount++;
+          if (branch === "branch1") {
+            if (gender === "Male") jyMale++;
+            else if (gender === "Female") jyFemale++;
+          } else if (branch === "branch2") {
+            if (gender === "Male") ksMale++;
+            else if (gender === "Female") ksFemale++;
           }
         });
+        setJyData({ maleCount: jyMale, femaleCount: jyFemale });
+        setKsData({ maleCount: ksMale, femaleCount: ksFemale });
 
-        setMaleCount(maleCount);
-        setFemaleCount(femaleCount);
-        setActiveCount(activeCount);
-        setExpiredCount(expiredCount);
+       
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -74,59 +116,74 @@ const DashboardPage = () => {
   }, []);
 
   useEffect(() => {
-    if (genderChartRef.current && statusChartRef.current) {
-      if (genderChartInstance.current) {
-        genderChartInstance.current.destroy();
+    const updateChart = (chartRef, chartInstance, data) => {
+      if (chartRef.current) {
+        if (chartInstance.current) {
+          chartInstance.current.destroy();
+        }
+        chartInstance.current = new Chart(chartRef.current, {
+          type: "pie",
+          data: {
+            labels: ["Male", "Female"],
+            datasets: [
+              {
+                data: [data.maleCount, data.femaleCount],
+                backgroundColor: ["blue", "pink"],
+              },
+            ],
+          },
+          options: { responsive: true },
+        });
       }
-      if (statusChartInstance.current) {
-        statusChartInstance.current.destroy();
+    };
+
+    updateChart(jyChartRef, jyChartInstance, jyData);
+    updateChart(ksChartRef, ksChartInstance, ksData);
+  }, [jyData, ksData]);
+
+  useEffect(() => {
+    if (earningsChartRef.current) {
+      if (earningsChartInstance.current) {
+        earningsChartInstance.current.destroy();
       }
 
-      const genderChartCanvas = genderChartRef.current;
-      genderChartInstance.current = new Chart(genderChartCanvas, {
-        type: "pie",
+      earningsChartInstance.current = new Chart(earningsChartRef.current, {
+        type: "bar",
         data: {
-          labels: ["Male", "Female"],
+          labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ],
           datasets: [
             {
-              data: [maleCount, femaleCount],
-              backgroundColor: ["blue", "pink"],
+              label: "Monthly Earnings",
+              data: monthlyEarnings,
+              backgroundColor: "rgba(54, 162, 235, 0.2)",
+              borderColor: "rgba(54, 162, 235, 1)",
+              borderWidth: 1,
             },
           ],
         },
         options: {
-          responsive: true,
-        },
-      });
-
-      const statusChartCanvas = statusChartRef.current;
-      statusChartInstance.current = new Chart(statusChartCanvas, {
-        type: "pie",
-        data: {
-          labels: ["Active", "Expired"],
-          datasets: [
-            {
-              data: [activeCount, expiredCount],
-              backgroundColor: ["green", "red"],
+          scales: {
+            y: {
+              beginAtZero: true,
             },
-          ],
-        },
-        options: {
-          responsive: true,
+          },
         },
       });
     }
-  }, [maleCount, femaleCount, activeCount, expiredCount]);
-
-  const handleSignOut = async () => {
-    try {
-      await SignOutUser();
-      navigate("/");
-      setCurrentUser();
-    } catch (error) {
-      console.error("Error signing out:", error);
-    }
-  };
+  }, [monthlyEarnings]);
   const handleSidebarOpen = () => {
     setIsSidebarOpen(true);
   };
@@ -140,17 +197,52 @@ const DashboardPage = () => {
       <Button onClick={handleSidebarOpen}>
         <FontAwesomeIcon icon={faBars} />
       </Button>
-      <Sidebar isOpen={isSidebarOpen} handleClose={handleSidebarClose} />
-      <Button onClick={handleSignOut}>
-        Logout
-        <FontAwesomeIcon icon={faArrowRightFromBracket} />
-      </Button>
+      <SidebarDashboard
+        isOpen={isSidebarOpen}
+        handleClose={handleSidebarClose}
+      />
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4} style={{ width: "100%", height: "300px" }}>
-          <canvas ref={genderChartRef}></canvas>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          style={{ height: "300px" }}
+          className="chart-ct"
+        >
+          <canvas ref={jyChartRef}></canvas>
         </Grid>
-        <Grid item xs={12} sm={4} style={{ width: "100%", height: "300px" }}>
-          <canvas ref={statusChartRef}></canvas>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          style={{ height: "300px" }}
+          className="chart-ct"
+        >
+          <canvas ref={ksChartRef}></canvas>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} style={{ height: "300px",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",marginTop:"30px" }} >
+          <Select
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(e.target.value)}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            {Array.from(
+              { length: new Date().getFullYear() - 2023 + 1 },
+              (_, i) => 2023 + i
+            ).map((year) => (
+              <MenuItem key={year} value={year}>
+                {year}
+              </MenuItem>
+            ))}
+          </Select>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <canvas ref={earningsChartRef}></canvas>
+          )}
         </Grid>
       </Grid>
     </div>
