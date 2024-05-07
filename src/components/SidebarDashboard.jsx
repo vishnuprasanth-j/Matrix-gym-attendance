5433b7e9540faa870aacc180ced81e40fc7f17bd
vishnuprasanth-j <jvpvis6@gmail.com>
2024-05-07 11:45:49 +0530

    updated dashboard page

--- src/components/SidebarDashboard.jsx
+++ src/components/SidebarDashboard.jsx
@@ -17,50 +17,51 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const SidebarDashboard = ({ isOpen, handleClose }) => {
-    const navigate = useNavigate();
-    const { branch } = useParams();
-    const { setCurrentUser } = useContext(AuthContext);
-    const handleSignOut = async () => {
-        try {
-          await SignOutUser();
-          navigate("/");
-          setCurrentUser();
-          handleClose();
-        } catch (error) {
-          console.error("Error signing out:", error);
-        }
-      };
-    
-    return(<>
-          <Backdrop
-            sx={{ zIndex: (theme) => theme.zIndex.drawer - 1 }}
-            open={isOpen}
-            onClick={handleClose}
-          />
-          <Drawer
-            open={isOpen}
-            onClose={handleClose}
-            sx={{
-              width: 250,
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: 250,
-              },
-            }}
-          >
-            <IconButton
-              sx={{ position: "absolute", top: 0, left: 0 }}
-              onClick={handleClose}
-            >
-              <FontAwesomeIcon icon={faTimes} />
-            </IconButton>
-    
-            <List
-              sx={{
-                marginTop: "50px",
-              }}
-            >
-              <ListItemButton
+  const navigate = useNavigate();
+  const { branch } = useParams();
+  const { setCurrentUser } = useContext(AuthContext);
+  const handleSignOut = async () => {
+    try {
+      await SignOutUser();
+      navigate("/");
+      setCurrentUser();
+      handleClose();
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  return (
+    <>
+      <Backdrop
+        sx={{ zIndex: (theme) => theme.zIndex.drawer - 1 }}
+        open={isOpen}
+        onClick={handleClose}
+      />
+      <Drawer
+        open={isOpen}
+        onClose={handleClose}
+        sx={{
+          width: 250,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 250,
+          },
+        }}
+      >
+        <IconButton
+          sx={{ position: "absolute", top: 0, left: 0 }}
+          onClick={handleClose}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </IconButton>
+
+        <List
+          sx={{
+            marginTop: "50px",
+          }}
+        >
+          {/* <ListItemButton
                 component={Link}
                 to={`/Dashboard/Earnings`}
                 onClick={handleClose}
@@ -73,15 +74,15 @@ const SidebarDashboard = ({ isOpen, handleClose }) => {
                 onClick={handleClose}
               >
                 <ListItemText primary="Stats" />
-              </ListItemButton>
-            <ListItemButton onClick={handleSignOut}>
-                <ListItemText primary="Logout" />
-                <FontAwesomeIcon icon={faArrowRightFromBracket} />
-              </ListItemButton>
-            </List>
-          </Drawer>
-        </>
-    );
+              </ListItemButton> */}
+          <ListItemButton onClick={handleSignOut}>
+            <ListItemText primary="Logout" />
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          </ListItemButton>
+        </List>
+      </Drawer>
+    </>
+  );
 };
 
-export default SidebarDashboard
+export default SidebarDashboard;
